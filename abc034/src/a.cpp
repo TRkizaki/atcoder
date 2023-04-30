ee238fde4218326beec7c8baef9dfca2dc51f2c4
TR Kizaki <trkizaki.web@gmail.com>
2023-04-30 16:02:45 +0400

    abc034 a

---
+++ abc034/src/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    string ans = (x < y) ? "Better" : "Worse";
+    cout << ans << endl;
+
+    return 0;
+}
