7948a83e46c3323288aade9b68a8cf7361f2dd0e
TR Kizaki <trkizaki.web@gmail.com>
2023-05-03 22:57:01 +0400

    abc041 a

---
+++ abc041/src/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int i;
+    cin >> s >> i;
+    char ans = s[i - 1];
+    cout << ans << endl;
+    return 0;
+}
