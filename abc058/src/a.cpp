514bebb25551dbba5638df7bfd03e58883911d48
TR Kizaki <trkizaki.web@gmail.com>
2023-05-31 14:38:34 +0400

    abc058 a

---
+++ abc058/src/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    string ans = ((b - a) == (c - b) ? "YES" : "NO");
+    cout << ans << endl;
+    return 0;
+}
