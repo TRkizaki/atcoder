80df53ea8efee55beac7c7da3a7a699035a86e95
TR Kizaki <trkizaki.web@gmail.com>
2023-06-06 18:22:15 +0400

    abc064 a

---
+++ abc064/src/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r, g, b;
+int main()
+{
+    cin >> r >> g >> b;
+
+    if ((r * 100 + b * 10 + c) % 4 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
